adf7449f737266a6a69c68d549343342f07302aa
Charles Conroy <conroycd@bc.edu>
2023-04-25 12:08:35 -0400

    Initial Commit

---
+++ Noteify/NoteifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteifyApp.swift
+//  Noteify
+//
+//  Created by Charley Conroy on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
